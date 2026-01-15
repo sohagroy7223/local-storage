3aa1b019e6c981a9f8b737364122f657dddd9a6b
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-15 21:29:55 +0600

    addad object to string by the json and back string to object by the json parse call and get the data

--- script/localstorage.js
+++ script/localstorage.js
@@ -27,7 +27,18 @@ const ObjectToLocalStorage = () => {
     friends: ["sumon", "kongkon", "ripon"],
   };
   //   console.log(person.toString());
+  // convert object to string by the json**********
   const converted = JSON.stringify(person);
-  console.log(converted);
+  // console.log(typeof converted);
+
+  // back string to object by the json**********
+  // const backToObject = JSON.parse(converted);
+  // console.log(backToObject.name);
+
   localStorage.setItem("akash", converted);
+  console.log(localStorage.getItem("akash"));
 };
+const data = localStorage.getItem("akash");
+// console.log(data);
+const convertData = JSON.parse(data);
+console.log(convertData.friends);
