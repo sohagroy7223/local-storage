ccf5c3832e8804ee3fa6788bee9b619123a652e3
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-15 20:39:08 +0600

    addad local storage set item an object

--- script/localstorage.js
+++ script/localstorage.js
@@ -5,10 +5,29 @@
 // localStorage.setItem("dream", "a freelancer");
 // console.log(localStorage.getItem("name"));
 // console.log(localStorage.removeItem("name"));
+// localStorage.setItem("lion", "20");
 
-const handleAddToStorage = () => {
-  localStorage.setItem("name", " sohag");
-  localStorage.setItem("age", 20);
-  console.log(localStorage.getItem("name"));
-  localStorage.removeItem("age");
+// localStorage.getItem("lion");
+
+// permeative data in local storage********
+
+// const handleAddToStorage = () => {
+//   localStorage.setItem("name", " sohag");
+//   localStorage.setItem("age", 20);
+//   console.log(localStorage.getItem("name"));
+//   localStorage.removeItem("age");
+// };
+
+// non-permeative data in local storage*******
+
+const ObjectToLocalStorage = () => {
+  const person = {
+    name: "akash",
+    age: 20,
+    friends: ["sumon", "kongkon", "ripon"],
+  };
+  //   console.log(person.toString());
+  const converted = JSON.stringify(person);
+  console.log(converted);
+  localStorage.setItem("akash", converted);
 };
