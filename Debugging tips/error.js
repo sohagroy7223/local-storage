6d7f6854ab11e6381a1860efeb2b9d4b0390bc52
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-17 17:15:41 +0600

    addad maney error and how to degugging in js

---
+++ Debugging tips/error.js
@@ -0,0 +1,67 @@
+// SyntaxError**********
+
+// for (Let i = 0; i < 5; i++) console.log(i);
+
+// const for = 0;
+
+// console.log("hello world"
+
+// const person = {
+//     name: "sohag",
+//     age: 20
+// console.log(person)
+
+// let x= "sohag
+// console.log(x)
+
+// TypeError**********
+
+// let a = 10;
+// a();
+
+// let user;
+// console.log(user.name);
+
+// ReferenceError************
+
+// const sum = () => {
+//   const result = 10 + 20;
+// };
+// console.log(result);
+
+// console.log(s);
+
+// console *************
+
+// console.log("sohag");
+// console.error("sohag");
+// console.warn("sohag");
+// const person = {
+//   name: "sohag",
+//   age: 20,
+// };
+// console.dir(person);
+// const numbers = [12, 4, 53, 56, 23];
+// console.dir(numbers);
+// console.table(numbers);
+
+// find console log console.error*********
+
+// const num1 = 10;
+// const num2 = undefined;
+// const result = num1 + num2;
+// console.log(num1, num2);
+// console.log(result);
+
+// we can debugging in sources by the break points********
+
+// for (let i = 0; i < 10; i++) {
+//   const element = i;
+//   console.log(element);
+// }
+// we can use debugger for break*****
+// for (let i = 0; i < 10; i++) {
+//     debugger
+//   const element = i;
+//   console.log(element);
+// }
